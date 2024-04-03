6f8c9588f7a7d8463644c8a0d0b7fb3b3d16eaba
NdegeFelix <findech6@gmail.com>
2024-04-03 21:13:42 +0300

    django plpsite update

--- plpblog/plpapp/views.py
+++ plpblog/plpapp/views.py
@@ -8,4 +8,4 @@ from django.http import HttpResponse
 def plpapp(request):
     return HttpResponse("Hello, World this is my first django project!")
 def contact(request):
-    return HttpResponse("Contact page")
+    return HttpResponse(" My Contact page")
